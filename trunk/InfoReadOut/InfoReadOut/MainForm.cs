183a65f8905030901f3291c29f62bfaf8f6fe95d
seraphlivery@gmail.com <seraphlivery@gmail.com@6b8df87c-40d4-0d93-d7c5-ae4b3b573d6b>
2012-03-18 12:39:08 +0000

    Upload InfoReadOut

---
+++ trunk/InfoReadOut/InfoReadOut/MainForm.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace InfoReadOut
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button_OpenFile_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                System.IO.StreamReader sr = new
+                System.IO.StreamReader(openFileDialog.FileName);
+                MessageBox.Show(openFileDialog.FileName);
+                
+                sr.Close();
+            }
+
+        }
+
+        private void button_OpenFolder_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                MessageBox.Show(folderBrowserDialog.SelectedPath);
+            }
+        }
+    }
+}
